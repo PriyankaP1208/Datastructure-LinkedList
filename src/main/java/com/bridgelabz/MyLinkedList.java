ebd58f79a0cd77ef0e14f38afbfb8951f890acf7
PriyankaP1208 <ppriyapatil1208@gmail.com>
2021-06-10 20:42:55 +0530

    ADDED:Adding elements

---
+++ src/main/java/com/bridgelabz/MyLinkedList.java
@@ -0,0 +1,27 @@
+package com.bridgelabz;
+
+public class MyLinkedList<K extends Comparable<K>> {
+    public INode<K> head;
+    public INode<K> tail;
+    public MyLinkedList()
+    {
+        this.head=head;
+        this.tail=tail;
+    }
+    public void add(INode<K> myNode)
+    {
+        if (this.tail==null)
+        {
+            this.tail=myNode;
+        }
+        if (this.head==null)
+        {
+            this.head=myNode;
+        }
+        else {
+            INode tempNode=this.head;
+            this.head=myNode;
+            this.head.setNext(tempNode);
+        }
+    }
+}
